d135473945f03e65028b328b1145b028894e8b2e
Mayar Adel <mayaradel92@gmail.com>
2022-08-06 23:45:18 +0200

    Initial Commit

---
+++ navigator1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  navigator1
+//
+//  Created by Mayar Adel on 8/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
